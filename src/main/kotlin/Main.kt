4416d2374890eead99bae61b8014ecd9d3a1fe8b
MartiVilas <marti.vilas14@gmail.com>
2023-12-19 11:20:23 +0100

    Quart: Versio 1.0.2, Calcul del preu de la furgoneta introduit i inici de actualitzacio de preu segons kilometratge

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,6 @@ package org.example
  * Este código calcula utiliza la función calcPreuFurgo en el cual tu has de introducir un número entre uno o dos
  * para que la función lea que tipo de furgoneta tienes en el caso de introducir 1 te dará el precio base de la Volkswagen
  * Grand California, en caso de poner 2 te dara el precio actualizado de la versión full equip de esta.
- *
  * @author Martí Vilàs Ruano
  * @version 1.0.2
  * @see calcPreuFurgo
