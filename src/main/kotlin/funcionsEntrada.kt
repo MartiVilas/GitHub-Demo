d2270132c8d2ed32d8b146ee5d512b04e14a83a3
MartiVilas <marti.vilas14@gmail.com>
2024-01-08 18:24:52 +0100

    add functions on funcionsEntrada.kt

--- src/main/kotlin/funcionsEntrada.kt
+++ src/main/kotlin/funcionsEntrada.kt
@@ -1,15 +1,29 @@
 package org.example
 import java.util.*
-fun llegirInt():Int {
+fun llegirInt(pMissatgeEntrada:String, pMissatgeError1: String, pMissatgeError2: String, pMin: Int, pMax: Int ):Int {
     var scanner=Scanner(System.`in`)
     var num:Int=0
     var error = true
     do {if (scanner.hasNextInt()){
         num=scanner.nextInt()
         error=false
-        }else {
+    }else {
         scanner.nextLine()
-        }
+    }
+    }while (error)
+    return num
+}
+
+fun llegirIntMissatge(pMissatgeEntrada:String, pMissatgeError: String ):Int {
+    var scanner=Scanner(System.`in`)
+    var num:Int=0
+    var error = true
+    do {if (scanner.hasNextInt()){
+        num=scanner.nextInt()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
     }while (error)
     return num
 }
@@ -29,7 +43,7 @@ fun llegirDouble():Double {
     return num
 }
 
-fun llegirFloat():Float {
+fun llegirFloat( pMissatgeEntrada: String, pMissatgeError: String):Float {
     var scanner=Scanner(System.`in`)
     var num:Float = 0.0f
     var error = true
@@ -55,4 +69,38 @@ fun llegirLong():Long {
     }
     }while (error)
     return num
+}
+
+fun llegirWord(pMessageIn: String, pMessageErrorDT: String): String {
+    var outputValue: String
+    do {
+        println(pMessageIn)
+        if (!scanner.hasNext()) {
+            println("ERROR: $pMessageErrorDT")
+            scanner.next()
+        } else {
+            outputValue = scanner.next()
+            scanner.nextLine()
+            return outputValue
+        }
+    } while (true)
+}
+
+fun llegirChar(pMessageIn: String, pMessageErrorDT: String, pCase: Int = -1): Char {
+    var outputValue: Char
+    do {
+        println(pMessageIn)
+        if (!scanner.hasNext()) {
+            println("ERROR: $pMessageErrorDT")
+            scanner.next()
+        } else {
+            outputValue = scanner.next()[0]
+            scanner.nextLine() // clear buffer
+            when (pCase) {
+                0 -> return outputValue.lowercaseChar()
+                1 -> return outputValue.uppercaseChar()
+                else -> return outputValue
+            }
+        }
+    } while (true)
 }
